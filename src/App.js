fe50cd80e63a2baef0c9fa48ad350ea19958c313
Chathuranga Wijesinghe <crwijesinghe@gmail.com>
2020-02-05 17:02:01 +0800

    35 & 36

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Person from "./Person/Person";
+import person from "./Person/Person";
 
 function App() {
   return (
     <div className="App">
       <h1>Hi, I'm a react app...</h1>
       <p>This is really working!</p>
-      <Person />
-      <Person />
-      <Person />
+      <Person name="Max" age="28" />
+      <Person name="Manu" age="29">My Hobbies: Racing</Person>
+      <Person name="Stephanie" age="26" />
     </div>
   );
   // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'I\'m a react app...'));
