ae4ec556c27a275214adcbda28a2e886436109cc
Chathuranga Wijesinghe <crwijesinghe@gmail.com>
2020-02-05 16:50:05 +0800

    34

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ function App() {
       <h1>Hi, I'm a react app...</h1>
       <p>This is really working!</p>
       <Person />
+      <Person />
+      <Person />
     </div>
   );
   // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'I\'m a react app...'));
