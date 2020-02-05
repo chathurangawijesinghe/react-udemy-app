9c5ef20c8fd696ce8cfdb6db38a46655b541f293
Chathuranga Wijesinghe <crwijesinghe@gmail.com>
2020-02-05 16:44:32 +0800

    32

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Person from "./Person/Person";
 
 function App() {
   return (
     <div className="App">
       <h1>Hi, I'm a react app...</h1>
+      <p>This is really working!</p>
+      <Person />
     </div>
   );
   // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'I\'m a react app...'));
