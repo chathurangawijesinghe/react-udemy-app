fdbd782d843c8cc93f7efa4c094f2019eb6d9023
Chathuranga Wijesinghe <crwijesinghe@gmail.com>
2020-03-19 17:03:53 +0800

    59

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import Person from "./Person/Person";
 class App extends Component {
   state = {
     persons: [
-      { name: "Max", age: 28 },
-      { name: "Manu", age: 29 },
-      { name: "Stephanie", age: 26 }
+      { id: 1, name: "Max", age: 28 },
+      { id: 2, name: "Manu", age: 29 },
+      { id: 3, name: "Stephanie", age: 26 }
     ],
     otherState: 'Some other value',
     showPersons: false
@@ -54,7 +54,8 @@ class App extends Component {
               name={person.name}
               age={person.age}
               click={this.deletePersonHandler.bind(this, index)}
-              changed={this.nameChangedHandler}
+              //changed={this.nameChangedHandler}
+              key={person.id}
             />
           })}
         </div>
